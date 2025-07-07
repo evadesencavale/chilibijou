59c84b46c932b876020626f42ce48d1caed63646
madmoune <madmoune@hotmail.ca>
2025-07-07 14:45:42 -0400

    Initial setup of syllogimous.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SyllogimousService } from './syllogimous/services/syllogimous.service';
+import { Question } from './syllogimous/models/question.models';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'er-training';
+  question: Question;
 
-  constructor() {}
+  constructor(private syllogimousService: SyllogimousService) {
+    this.question = syllogimousService.createRandomQuestion(3, true);
+    console.log(this.question);
+  }
 }
---
+++ src/app/syllogimous/models/stats.models.ts
@@ -0,0 +1,50 @@
+import { EnumQuestionType } from "../constants/question.constants";
+
+export class TypeBasedStats {
+    [EnumQuestionType.Syllogism] = new TypeBasedStatsInner(EnumQuestionType.Syllogism);
+    [EnumQuestionType.Distinction] = new TypeBasedStatsInner(EnumQuestionType.Distinction);
+    [EnumQuestionType.ComparisonNumerical] = new TypeBasedStatsInner(EnumQuestionType.ComparisonNumerical);
+    [EnumQuestionType.ComparisonChronological] = new TypeBasedStatsInner(EnumQuestionType.ComparisonChronological);
+    [EnumQuestionType.LinearArrangement] = new TypeBasedStatsInner(EnumQuestionType.LinearArrangement);
+    [EnumQuestionType.CircularArrangement] = new TypeBasedStatsInner(EnumQuestionType.CircularArrangement);
+    [EnumQuestionType.Direction] = new TypeBasedStatsInner(EnumQuestionType.Direction);
+    [EnumQuestionType.Direction3DSpatial] = new TypeBasedStatsInner(EnumQuestionType.Direction3DSpatial);
+    [EnumQuestionType.Direction3DTemporal] = new TypeBasedStatsInner(EnumQuestionType.Direction3DTemporal);
+    [EnumQuestionType.GraphMatching] = new TypeBasedStatsInner(EnumQuestionType.GraphMatching);
+    [EnumQuestionType.Analogy] = new TypeBasedStatsInner(EnumQuestionType.Analogy);
+    [EnumQuestionType.Binary] = new TypeBasedStatsInner(EnumQuestionType.Binary);
+}
+
+export class TypeBasedStatsInner {
+    type: EnumQuestionType;
+    completed = 0;
+    accuracy = 0;
+    stats = {
+        "2": new TypeBasedStatsInner2(),
+        "3": new TypeBasedStatsInner2(),
+        "4": new TypeBasedStatsInner2(),
+        "5": new TypeBasedStatsInner2(),
+        "6+": new TypeBasedStatsInner2(),
+    };
+    
+    constructor(type: EnumQuestionType) {
+        this.type = type;
+    }
+}
+
+export class TypeBasedStatsInner2 {
+    sum = 0;
+    count = 0;
+    fastest = 0;
+    slowest = 0;
+    correct = 0;
+    incorrect = 0;
+    timeout = 0;
+    last10Sum = 0;
+    last10Count = 0;
+    last10Fastest = 0;
+    last10Slowest = 0;
+    last10Correct = 0;
+    last10Incorrect = 0;
+    last10Timeout = 0;
+}
