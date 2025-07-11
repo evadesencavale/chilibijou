f02cfe095e875943d25b686069584b94dc107f39
madmoune <madmoune@hotmail.ca>
2025-07-11 14:32:42 -0400

    Initial setup

---
+++ src/app/training/association/association-component/association-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-association-component',
+  imports: [],
+  templateUrl: './association-component.html',
+  styleUrl: './association-component.scss'
+})
+export class AssociationComponent {
+
+}
